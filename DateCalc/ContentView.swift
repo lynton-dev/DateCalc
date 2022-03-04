c31adaee333ae7e56cf1d3bbfab959600738d7c1
lynton-dev <61889203+lynton-dev@users.noreply.github.com>
2022-03-04 00:30:36 -0700

    - Added support for time zone when converting between unix time and human readable dates - Added "Release" build scheme for the app

--- DateCalc/ContentView.swift
+++ DateCalc/ContentView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var unixTimeText = GetUnixTimeString()
+    @State private var unixTimeText = GetCurrentUnixTimeString()
     @State private var date = Date()
     @State private var seconds = "0"
-    let calendar = Calendar.current
+    @State private var previousSeconds = "0"
+    @State private var timeZoneSelection = TimeZone.current.identifier
+    @State private var previousTimeZoneSelection = TimeZone.current.identifier
+    let timeZoneIdentifiers = TimeZone.knownTimeZoneIdentifiers
     
     var body: some View {
         HStack {
@@ -24,6 +27,7 @@ struct ContentView: View {
                     TextField("Enter unix time", text: $unixTimeText)
                     .frame(width: 150)
                     
+                    // Reset unix time button
                     Button {
                         self.unixTimeText = GetUnixTimeString()
                     } label: {
@@ -36,8 +40,7 @@ struct ContentView: View {
                 
                 // Convert from unix time to human readable timestamp
                 Button {
-                    self.date = Date(timeIntervalSince1970: Double(unixTimeText) ?? 0)
-                    self.seconds = String(calendar.component(.second, from: date))
+                    ConvertUnixTimeToHumanDate()
                 } label: {
                     Image(systemName: "arrow.right.circle.fill")
                 }
@@ -59,11 +62,32 @@ struct ContentView: View {
                             selection: $date,
                             displayedComponents: [.date, .hourAndMinute]
                     )
+                    .onAppear()
+                    {
+                        // Make sure the unix time and this date match up on launch
+                        ConvertUnixTimeToHumanDate()
+                    }
                     .datePickerStyle(.field)
                     .frame(width: 235)
                     
+                    // Seconds text field
                     TextField("", text: $seconds)
+                    .onChange(of: self.seconds) { newValue in
+                        // Seconds field validation
+                        if (self.seconds.isEmpty || !self.seconds.isNumber) {
+                            self.seconds = "0"
+                            return
+                        }
+                        
+                        let secondsInt = Int(self.seconds) ?? 0
+                        if (secondsInt < 0 || secondsInt >= 60) {
+                            self.seconds = self.previousSeconds
+                        } else {
+                            self.previousSeconds = self.seconds
+                        }
+                    }
                     .onAppear() {
+                        // Extract seconds from date and update seconds text field
                         UpdateSecondsFromDate()
                     }
                     .frame(width: 25)
@@ -71,6 +95,7 @@ struct ContentView: View {
                     Text("sec")
                         .padding(.leading, -5)
                     
+                    // Reset date button
                     Button {
                         ResetDate()
                     } label: {
@@ -80,18 +105,24 @@ struct ContentView: View {
                     .padding(.leading, 5)
                 }
                 .padding(.bottom, 10)
+                
+                // Select Time Zone
+                Picker("Time Zone", selection: $timeZoneSelection) {
+                    ForEach(self.timeZoneIdentifiers, id: \.self) {
+                        Text($0)
+                    }
+                }.onChange(of: self.timeZoneSelection) { newValue in
+                    // Adjust date from previously selected time zone to the newly selected time zone
+                    self.date = self.date.convert(from: TimeZone(identifier: self.previousTimeZoneSelection)!, to: TimeZone(identifier: self.timeZoneSelection)!)
+                    
+                    // Store this as our new previous time zone selection
+                    self.previousTimeZoneSelection = self.timeZoneSelection
+                }
+                .padding(.bottom, 10)
             
                 // Convert from human readable timestamp to unix time
                 Button {
-                    var components = DateComponents()
-                    components.day = calendar.component(.day, from: self.date)
-                    components.month = calendar.component(.month, from: self.date)
-                    components.year = calendar.component(.year, from: self.date)
-                    components.hour = calendar.component(.hour, from: self.date)
-                    components.minute = calendar.component(.minute, from: self.date)
-                    components.second = Int(seconds)
-                    self.date = calendar.date(from: components) ?? Date()
-                    unixTimeText = GetUnixTimeString(dateParam: self.date)
+                    ConvertHumanDateToUnixTime()
                 } label: {
                     Image(systemName: "arrow.left.circle.fill")
                 }
@@ -109,9 +140,52 @@ struct ContentView: View {
         UpdateSecondsFromDate()
     }
     
+    func ConvertUnixTimeToHumanDate() {
+        // Set date field. Take into account Time Zone.
+        // First get date object based on the unix time text field
+        let unixDateTime = Date(timeIntervalSince1970: Double(unixTimeText) ?? 0)
+        // Then adjust the date to the local time zone. We will always deal with unix time in our local time zone for simplicity.
+        self.date = unixDateTime.convert(from: TimeZone.current, to: TimeZone(identifier: self.timeZoneSelection) ?? TimeZone.current)
+        
+        // Set seconds field
+        let calendar = Calendar.current
+        self.seconds = String(calendar.component(.second, from: self.date))
+    }
+    
+    func ConvertHumanDateToUnixTime() {
+        var calendar = Calendar.current
+        let components = GetComponentsFromDate(dateParam: self.date, secondsParam: self.seconds)
+        calendar.timeZone = TimeZone(identifier: self.timeZoneSelection) ?? TimeZone.current
+        self.date = calendar.date(from: components) ?? Date()
+        unixTimeText = GetUnixTimeString(dateParam: self.date)
+    }
+    
+    func GetComponentsFromDate(dateParam: Date, secondsParam: String? = "0") -> DateComponents
+    {
+        var calendar = Calendar.current
+        calendar.timeZone = TimeZone(identifier: self.timeZoneSelection) ?? TimeZone.current
+        var components = DateComponents()
+        components.day = calendar.component(.day, from: dateParam)
+        components.month = calendar.component(.month, from: dateParam)
+        components.year = calendar.component(.year, from: dateParam)
+        components.hour = calendar.component(.hour, from: dateParam)
+        components.minute = calendar.component(.minute, from: dateParam)
+        components.second = Int(secondsParam ?? "0")
+        return components
+    }
+    
     func UpdateSecondsFromDate()
     {
+        let calendar = Calendar.current
         self.seconds = String(calendar.component(.second, from: self.date))
+    }
+    
+    func GetUnixTimeString(dateParam: Date? = nil) -> String {
+        var date = dateParam ?? Date()
+        // Adjust date to our local time zone. We will always deal with unix time in our local time zone for simplicity.
+        date = date.convert(from: TimeZone(identifier: self.timeZoneSelection) ?? TimeZone.current, to: TimeZone.current)
+        
+        return String(format: "%.0f", date.timeIntervalSince1970.rounded())
     }
 }
 
@@ -121,7 +195,20 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-func GetUnixTimeString(dateParam: Date? = nil) -> String {
+func GetCurrentUnixTimeString(dateParam: Date? = nil) -> String {
     let date = dateParam ?? Date()
     return String(format: "%.0f", date.timeIntervalSince1970.rounded())
+}
+
+extension Date {
+    func convert(from initTimeZone: TimeZone, to targetTimeZone: TimeZone) -> Date {
+        let delta = TimeInterval(targetTimeZone.secondsFromGMT(for: self) - initTimeZone.secondsFromGMT(for: self))
+        return addingTimeInterval(delta)
+    }
+}
+
+extension String  {
+    var isNumber: Bool {
+        return !isEmpty && rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+    }
 }
